323d6999d8945a22745fcd5630d35b7e7d83d6e7
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 12:02:48 -0700

    Import file and split into list of words

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,18 @@
 # put your code here.
+def count_words(my_file):
+    """
+    """
+    text = []
+    with open(my_file) as text_file: # import the file
+        
+        for line in text_file:    
+            line = line.rstrip()
+            words = line.split(' ')  # split each word by a space
+            text.extend(words)
+        return text
+        # initialize empty dictionary
+    # loop over list
+        # use dictionary.get('word',0) + 1 to track count of words
+    # print key, values
+        # use .items()
+print count_words('test.txt')
