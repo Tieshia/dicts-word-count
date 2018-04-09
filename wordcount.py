d68c233e83aa5e7aeb374e188280bb167c075f7e
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 12:10:05 -0700

    Adjusted for larger text file.

--- wordcount.py
+++ wordcount.py
@@ -1,11 +1,13 @@
 # put your code here.
 def count_words(my_file):
     """
+        Prints number of times a word appears in a text.
     """
+
     text = []
-    with open(my_file) as text_file: # import the file
-        
-        for line in text_file:    
+    with open(my_file) as text_file:  # import the file
+
+        for line in text_file:
             line = line.rstrip()
             words = line.split(' ')  # split each word by a space
             text.extend(words)
@@ -15,6 +17,10 @@ def count_words(my_file):
     for word in text:
         # use dictionary.get('word',0) + 1 to track count of words
         word_count[word] = word_count.get(word, 0) + 1
-    # print key, values
-        # use .items()
-print count_words('test.txt')
+    # print key, values;  use .iteritems()
+    for key, value in word_count.iteritems():
+        print key, value
+
+        
+count_words('test.txt')
+count_words('twain.txt')
