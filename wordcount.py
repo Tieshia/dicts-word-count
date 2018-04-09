5a9f35aa0004a70e7996c7aa0ad56e0f714db408
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 12:50:34 -0700

    Stripped punctuation

--- wordcount.py
+++ wordcount.py
@@ -1,19 +1,28 @@
+import string
+
 # put your code here.
+
+
 def count_words(my_file):
     """
         Prints number of times a word appears in a text.
     """
 
     word_count = {}  # initialize empty dictionary
+    # punctuation = ['"', '_', '*', ',', ':', ';', '!', '?', '.']
 
     with open(my_file) as text_file:  # import the file
 
         for line in text_file:
-            line = line.rstrip()
+            line = line.lower()  # lowercase line
             words = line.split()  # split each word by a space
+
             # loop over list
             for word in words:
+                word = word.strip(string.punctuation)  # strip out punctuation
+
                 # use dictionary.get('word',0) + 1 to track count of words
+
                 word_count[word] = word_count.get(word, 0) + 1
 
     # print key, values;  use .iteritems()
